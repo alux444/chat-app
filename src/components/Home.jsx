79e6cd66800d821275d103fad739c0910c2620ff
alux444 <aluux999@gmail.com>
2023-05-27 18:28:14 +1200

    fixed session logic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Box, Button } from "@mui/material";
+import { AccountContext } from "./AccountContext";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { user, setUser } = useContext(AccountContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setUser({ loggedIn: false, username: null });
+    navigate("/");
+  };
+
   return (
     <Box>
-      <p>hello</p>
+      <Button onClick={handleLogout}>Logout?</Button>
+      <p>hello, {user.username}</p>
     </Box>
   );
 };
